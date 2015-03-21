004a554df791d60ef4d8381d7556a71b72545a1c
Joanne Yae <yae.joanne@gmail.com>
2015-03-21 13:59:14 -0400

    Redid the high charts

--- cloud/models/btc/charts.php
+++ cloud/models/btc/charts.php
@@ -27,10 +27,11 @@
 	function getCData(){
 
 
-		$news = dbMassData("SELECT * FROM news WHERE dateT !='0000-00-00 00:00:00' GROUP BY dateT ORDER BY dateT ASC");
-		$prices = dbMassData("SELECT * FROM minuteCoin WHERE tDate != '0000-00-00 00:00:00' GROUP BY tDate ORDER BY timestamp DESC LIMIT 100");
-
-		$resp = array("news"=>$news, "prices"=>$prices);
+		//$news = dbMassData("SELECT * FROM news WHERE dateT !='0000-00-00 00:00:00' GROUP BY dateT ORDER BY dateT ASC");
+		//$prices = dbMassData("SELECT * FROM minuteCoin WHERE tDate != '0000-00-00 00:00:00' GROUP BY tDate ORDER BY timestamp DESC LIMIT 100");
+		$last_price = dbMassData("SELECT * FROM bitfinex WHERE last_price != 0 AND bid != 0 ORDER BY timestamp DESC LIMIT 240");
+		//$resp = array("news"=>$news, "prices"=>$prices);
+		$resp = array("last_price"=>$last_price);
 
 		return $resp;
 	}
--- cloud/models/btc/minutePrices.php
+++ cloud/models/btc/minutePrices.php
@@ -30,7 +30,7 @@
 		//$news = dbMassData("SELECT * FROM news WHERE dateT !='0000-00-00 00:00:00' GROUP BY dateT ORDER BY dateT ASC");
 		//$prices = dbMassData("SELECT * FROM minuteCoin WHERE tDate != '0000-00-00 00:00:00' AND current != 0 AND bid != 0 ORDER BY timestamp DESC LIMIT 240");
 		$last_price = dbMassData("SELECT * FROM bitfinex WHERE last_price != 0 AND bid != 0 ORDER BY timestamp DESC LIMIT 240");
-		//var = darabasename(SELECT FROM bitfinex where bid!=0 ORDER BY timestamp DESC LIMIT 240)
+		//var = databasename(SELECT FROM bitfinex where bid!=0 ORDER BY timestamp DESC LIMIT 240)
 		$resp = array("last_price"=>$last_price);
 
 		return $resp;
