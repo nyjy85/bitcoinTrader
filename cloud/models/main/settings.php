a3691808a9e0c7ad3ad20242f17e7197fb3d18be
Joanne Yae <yae.joanne@gmail.com>
2015-03-30 16:42:31 -0400

    gitignore edit

---
+++ cloud/models/main/settings.php
@@ -0,0 +1,11 @@
+<?php
+//your username here for MYSQL
+$username= "adminqrYyzgY";
+
+//your password here for MYSQL
+$dbPassword = "W5W7-sXQnbCx";
+
+//keep database as BTC
+$database= "bitproj";
+
+?>
