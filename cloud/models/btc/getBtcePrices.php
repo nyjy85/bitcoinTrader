9727e3c2871b18ba35dc0ab0b303303194700401
Joanne Yae <yae.joanne@gmail.com>
2015-03-30 23:21:11 -0400

    added BTCE prices

--- cloud/models/btc/getBtcePrices.php
+++ cloud/models/btc/getBtcePrices.php
@@ -20,19 +20,21 @@
 	
 			//echo($theDate);
 			//echo("http://www.quandl.com/api/v1/datasets/BITCOIN/BITSTAMPUSD?auth_token=6sQU_EYPwHRMkJsReFG9");
-			$info = file_get_contents("https://api.exchange.coinbase.com/products/btc-usd/ticker");
+			$info = file_get_contents("https://btc-e.com/api/3/ticker/btc_usd");
 			$tInfo = json_decode($info, true);
 
-			$price = $tInfo['price'];
-	
+			$price = $tInfo['btc_usd']['last'];
+			$ask = $tInfo['btc_usd']['buy'];
+			$bid = $tInfo['btc_usd']['sell'];
+			$volume = $tInfo['btc_usd']['vol'];
 
 
 
 
 			
-			dbQuery("INSERT INTO coinbase (price) VALUES ($price)");
+			dbQuery("INSERT INTO btcec (price, ask, bid, volume) VALUES ($price, $ask, $bid, $volume)");
 				//echo("INSERT INTO btcHistory (open, high, low, close, volume, volumeUSD, weightedPrice, avg, tDate) VALUES ($open, $high, $low, $close, $volume, $volumeC, $weightedPrice, $avg, '$theDate')");
-			echo("INSERT INTO coinbase (price) VALUES ($price)");
+			echo("INSERT INTO btcec (price, ask, bid, volume) VALUES ($price, $ask, $bid, $volume)");
 		//return true  
 		return $tInfo;
 	}
