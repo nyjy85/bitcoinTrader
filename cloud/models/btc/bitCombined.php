bd17493d79fe2f7bd674974da56017160fd22a8d
Joanne Yae <yae.joanne@gmail.com>
2015-04-07 21:49:05 -0400

    included btc-e prices in the chart

--- cloud/models/btc/bitCombined.php
+++ cloud/models/btc/bitCombined.php
@@ -13,9 +13,12 @@
 		
 		$resp = dbMassData("SELECT * FROM (SELECT * FROM bitfinex ORDER BY timestamp DESC LIMIT 240) t ORDER BY timestamp ASC");
 		$resp1 = dbMassData("SELECT * FROM (SELECT * FROM bitStamp ORDER BY timestamp DESC LIMIT 240) t ORDER BY timestamp ASC");
+		$resp2 = dbMassData("SELECT * FROM (SELECT * FROM btcec WHERE price != 0 AND bid != 0 ORDER BY timestamp DESC LIMIT 240) t ORDER BY timestamp ASC"); //gets the last 240 rows then sorts them in asecending order
+
 		$allResp = array(); // sets up an empty array
 		$allResp[0] =  $resp; // assigns the bifinex db to the 0th index
 		$allResp[1]= $resp1; // assigns the bitStamp db to the 1st index
+		$allResp[2] = $resp2; // assigns the btcec db to the 2nd index
 		return $allResp; // returns the array
 	}
 ?>
